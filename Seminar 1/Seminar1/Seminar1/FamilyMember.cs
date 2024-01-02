6b13a30c017e35e1017536b94b1944ca2a103237
Yaroslaw-aw <manumission@mail.ru>
2024-01-02 21:18:11 +0300

    Seminar 1 ready

---
+++ Seminar 1/Seminar1/Seminar1/FamilyMember.cs
@@ -0,0 +1,150 @@
+﻿namespace Seminar1
+{
+    enum Gender { Male, Female }
+    internal class FamilyMember
+    {
+        public string name { get; set; }
+        public Gender gender { get; set; }
+        public FamilyMember[] children { get; set; }
+        public FamilyMember mother { get; set; }
+        public FamilyMember father { get; set; }
+
+        public FamilyMember()
+        {
+        }
+
+        public FamilyMember(string name, Gender gender, FamilyMember mother, FamilyMember father, params FamilyMember[]? familyMembers)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.mother = mother;
+            this.father = father;
+            this.children = familyMembers;
+        }
+
+        public void MothersInFamily()
+        {
+            FamilyMember adult = this;
+
+            if (adult.mother != null)
+            {
+                adult = adult.children.Length > 0 && adult.children[0].mother != null ? adult.children[0].mother : this;
+            }
+
+            while (adult.mother != null)
+                adult = adult.mother;
+
+            if (adult.gender == Gender.Female)
+                Console.Write($"{adult.name} -> ");
+
+
+            bool femaleChild = true;
+            while (femaleChild)
+            {
+                femaleChild = false;
+                Console.Write($"{adult.name} -> ");
+
+                foreach (FamilyMember child in adult.children)
+                    if (child.gender == Gender.Female)
+                    {
+                        adult = child;
+                        femaleChild = true;
+                        break;
+                    }
+            }
+        }
+
+        public void PrintFamily()
+        {
+            FamilyMember secondMember = null;
+            if (this.children != null)
+                secondMember = this.gender == Gender.Male ? this.children[0].mother : this.children[0].father;
+            if (secondMember != null)
+                PrintFamily(this, secondMember);
+            else
+                PrintFamily(this);
+
+        }
+
+        private void PrintFamily(params FamilyMember[] familyMembers)
+        {
+            List<FamilyMember> children = new List<FamilyMember>();
+
+            foreach (FamilyMember familyMember in familyMembers)
+                Console.Write($"{familyMember.name} ");
+            Console.WriteLine();
+
+            foreach (FamilyMember familyMember in familyMembers)
+            {
+                if (familyMember.children != null)
+                {
+                    foreach (FamilyMember child in familyMember.children)
+                    {
+                        if (child.children != null)
+                        {
+                            foreach (FamilyMember child2 in child.children)
+                            {
+                                FamilyMember second = child.gender == Gender.Male ? child2.mother : child2.father;
+                                if (second != null && !children.Contains(second))
+                                    children.Add(second);
+                            }
+
+                        }
+                        if (!children.Contains(child))
+                            children.Add(child);
+                    }
+                }
+            }
+            if (children.Count > 0)
+                PrintFamily(children.ToArray());
+        }
+
+        public void ImmediateFamily()
+        {
+            FamilyMember member = this;
+            List<FamilyMember> brothersAndSisters = new List<FamilyMember>();
+
+
+            if (this.mother != null && this.father != null)
+            {
+                Console.WriteLine($"{this.mother.name} {this.father.name} ");
+            }
+
+            if (member.father.children != null)
+            {
+                foreach (var brotherAndSister in member.father.children)
+                {
+                    if (!brothersAndSisters.Contains(brotherAndSister))
+                    {
+                        brothersAndSisters.Add(brotherAndSister);
+                    }
+                }
+            }
+
+            if (member.mother.children != null)
+            {
+                foreach (var brotherAndSister in member.mother.children)
+                {
+                    if (!brothersAndSisters.Contains(brotherAndSister))
+                    {
+                        brothersAndSisters.Add(brotherAndSister);
+                    }
+                }
+            }
+
+            foreach (var brotherAndSister in brothersAndSisters)
+            {
+                Console.Write($"{brotherAndSister.name} ");
+            }
+
+            if (member.children != null)
+            {
+                foreach (var child in member.children)
+                {
+                    Console.Write($"{child.name} ");
+                }
+            }
+        }
+       
+    }
+}
---
+++ Seminar 1/Seminar1/Seminar1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace Seminar1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FamilyMember grandfather = new() { mother = null, father = null, name = "Дедушка", gender = Gender.Male };
+            FamilyMember grandmother = new() { mother = null, father = null, name = "Бабушка", gender = Gender.Female };
+            FamilyMember father = new FamilyMember() { mother = grandmother, father = grandfather, name = "Папа", gender = Gender.Male };
+            grandfather.children = new FamilyMember[] { father };
+            FamilyMember mother = new FamilyMember() { mother = null, father = null, name = "Мама", gender = Gender.Female };
+            FamilyMember mother2 = new FamilyMember() { father = null, mother = null, name = "Мама2", gender = Gender.Female };
+            FamilyMember son = new FamilyMember() { mother = mother, father = father, name = "Сын", gender = Gender.Male };
+            FamilyMember son2 = new FamilyMember() { father = father, mother = mother2, name = "Сын2", gender = Gender.Male };
+            mother2.children = new FamilyMember[] { son2 };
+
+
+            mother.children = new FamilyMember[] { son };
+            father.children = new FamilyMember[] { son, son2 };
+
+
+
+            //grandfather.PrintFamily();
+
+            son.ImmediateFamily();
+        }
+    }
+}
