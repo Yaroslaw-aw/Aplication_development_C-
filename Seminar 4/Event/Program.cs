4642db174f1075a1315f6a2a28a8d6de24bca364
Yaroslaw-aw <manumission@mail.ru>
2024-01-25 02:41:34 +0300

    Fucking calculator

---
+++ Seminar 4/Event/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Event
+{
+    public class MyEventArgs : EventArgs
+    {
+        public string? Message { get; set; }
+    }
+
+    public delegate void MeEventHandler (object sender, MyEventArgs args);
+
+    class ClassWithEvents
+    {
+        public event MeEventHandler SomeEvent;
+
+        protected void OnSomeEvent(MyEventArgs args)
+        {
+            SomeEvent?.Invoke(this, args);
+        }
+
+        public void DoSomeWork()
+        {
+            new Thread(
+                () =>
+                {
+                    Thread.Sleep(2000);
+                    OnSomeEvent(new MyEventArgs { Message = "Всё!" });
+                }
+                ).Start();
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassWithEvents newEvent = new ClassWithEvents();
+
+            newEvent.SomeEvent += NewEvent_SomeEvent;
+
+            newEvent.DoSomeWork();
+
+            Console.WriteLine("Запустили на выполнение");
+
+            Console.ReadLine();
+
+            newEvent.SomeEvent -= NewEvent_SomeEvent;
+
+        }
+
+        private static void NewEvent_SomeEvent(object sender, MyEventArgs args)
+        {
+            Console.WriteLine("Событие от класса " + sender + "Сообщающее " + args.Message);
+        }
+    }
+}
---
+++ Seminar5/FuckingCalculator/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using System.Windows;
+
+namespace FuckingCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator calculator;
+        public MainWindow()
+        {
+            InitializeComponent();
+            calculator = new Calculator();
+            calculator.Result += Calculator_Result;
+        }
+
+        private void Calculator_Result(object? sender, CalculatorArgs e)
+        {
+            Answer.Content = e.answer;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {               
+            string input = InputText.Text;
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (Char ch in input)
+            {
+                if ('0' <= ch && ch <= '9')
+                    result.Append(ch);
+            }
+
+            string? name = (e.Source as FrameworkElement)?.Name;
+
+            if (!int.TryParse(result.ToString(), out int value) && name != "Cancel")
+            {
+                MessageBox.Show("Ты че, пьяный?");
+                return;
+            }            
+
+            switch (name)
+            {
+                case "Add":
+                    calculator.Add(value);
+                    InputText.Text = string.Empty;
+                    break;
+                case "Sub":
+                    calculator.Sub(value);
+                    InputText.Text = string.Empty;
+                    break;
+                case "Mult":
+                    calculator.Mult(value);
+                    InputText.Text = string.Empty;
+                    break;
+                case "Div":
+                    calculator.Div(value);
+                    InputText.Text = string.Empty;
+                    break;
+                case "Cancel":
+                    InputText.Text = string.Empty;
+                    calculator.Cancel();
+                    break;
+                default:
+                    MessageBox.Show("Какая-то ошибка");
+                    InputText.Text = string.Empty;
+                    break;
+            }
+        }
+    }
+}
